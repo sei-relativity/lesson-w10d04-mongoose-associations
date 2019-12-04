0107d0c21a40ee52748cd862409e37ead5a1953b
Sager Alarifi <sagr.1993@gmail.com>
2019-12-04 13:35:18 +0300

    finish user schema

--- mongoose-associations-app/models/user.js
+++ mongoose-associations-app/models/user.js
@@ -1,17 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const tweetSchema = new Schema({
-  text: String,
-  date: Date
-});
+const tweetSchema = new Schema(
+  {
+    tweetText: String
+  },
+  { timestamps: true }
+);
 
-const userSchema = new Schema({
-  name: String,
-  // embed tweets in user
-  tweets: [tweetSchema]
-});
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      default: ""
+    },
+    tweets: [tweetSchema]
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 const Tweet = mongoose.model("Tweet", tweetSchema);
+
 module.exports = { User, Tweet };
--- mongoose-associations-app/server.js
+++ mongoose-associations-app/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+
 const Food = require("./models/food");
 const Ingredient = require("./models/ingredient");
+var User = require("./models/user").User;
+var Tweet = require("./models/user").Tweet;
 
 const mongoURI = "mongodb://localhost:27017/mongoRelationships";
 const port = 5000;
@@ -14,6 +17,8 @@ mongoose.connect(mongoURI, { useNewUrlParser: true }, () => {
 app.listen(port, () => {
   console.log("listening");
 });
+
+app.use(express.json());
 
 // send all information for all foods
 app.get("/api/foods/", (req, res) => {
@@ -27,4 +32,42 @@ app.get("/api/foods/", (req, res) => {
       console.log(`found and populated all foods: ${foods}`);
       res.json(foods);
     });
+});
+
+app.post("/api/users", (req, res) => {
+  User.create(req.body, (error, newUser) => {
+    res.json(newUser);
+  });
+});
+
+// create tweet embedded in user
+app.post("/api/users/:userId/tweets", (req, res) => {
+  // store new tweet in memory with data from request body
+  var newTweet = new Tweet({ tweetText: req.body.tweetText });
+
+  // find user in db by id and add new tweet
+  User.findById(req.params.userId, (error, foundUser) => {
+    foundUser.tweets.push(newTweet);
+    foundUser.save((err, savedUser) => {
+      res.json(newTweet);
+    });
+  });
+});
+
+// update tweet embedded in user
+app.put("/api/users/:userId/tweets/:id", (req, res) => {
+  // set the value of the user and tweet ids
+  const userId = req.params.userId;
+  const tweetId = req.params.id;
+
+  // find user in db by id
+  User.findById(userId, (err, foundUser) => {
+    // find tweet embedded in user
+    const foundTweet = foundUser.tweets.id(tweetId);
+    // update tweet text and completed with data from request body
+    foundTweet.tweetText = req.body.tweetText;
+    foundUser.save((err, savedUser) => {
+      res.json(foundTweet);
+    });
+  });
 });
