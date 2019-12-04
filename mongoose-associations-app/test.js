5fbeb1eb5dc1a5fff4263a577c8a09ea4d791974
Sager Alarifi <sagr.1993@gmail.com>
2019-12-04 13:21:57 +0300

    create test file

---
+++ mongoose-associations-app/test.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Food = require("./models/food");
+const Ingredient = require("./models/ingredient");
+
+const mongoURI = "mongodb://localhost:27017/mongooseAssociationsInClass";
+mongoose.connect(mongoURI, { useNewUrlParser: true }, () => {
+  console.log("the connection with mongod is established");
+});
+
+Food.findOne({ name: "Quiche" })
+  .populate("ingredients") // <- pull in ingredient data
+  .exec((err, food) => {
+    if (err) {
+      return console.log(err);
+    }
+    if (food.ingredients.length > 0) {
+      console.log(`I love ${food.name} for the ${food.ingredients[0].name}`);
+    } else {
+      console.log(`${food.name} has no ingredients.`);
+    }
+    console.log(`what was that food? ${food}`);
+  });
